2a2bed8f1f67cc69344f1f4b9e790310bfe23684
hellgrenj <hellgrenj@gmail.com>
2021-01-13 09:11:01 +0100

    minor addition to a comment :)

--- api/src/Application/Modules/TodoListModule/Domain/Models/TodoListItem.cs
+++ api/src/Application/Modules/TodoListModule/Domain/Models/TodoListItem.cs
@@ -9,7 +9,7 @@ namespace api.Application.Modules.TodoListModule.Domain.Models
         public bool Done { get; set; }
 
         // TodolistModule´s Domain Model now "cares about JSON", but thats slightly better then switching to Newtonsoft and use the reference loop support
-        // in a more complex app we would not return the domain model entity but rather map the domain model to a view model or similiar for the API and then leave out this navigation property
+        // in a more complex app we would not return the domain model entity but rather map the domain model to a DTO or view model or similiar for the API and then leave out this navigation property
         [JsonIgnore]
         public TodoList TodoList { get; set; }
     }
