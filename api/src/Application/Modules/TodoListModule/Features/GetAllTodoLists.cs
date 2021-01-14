1e314deace11e905ceb16d88dfea80daaee2c882
hellgrenj <hellgrenj@gmail.com>
2021-01-14 09:03:47 +0100

    some formatting

--- api/src/Application/Modules/TodoListModule/Features/GetAllTodoLists.cs
+++ api/src/Application/Modules/TodoListModule/Features/GetAllTodoLists.cs
@@ -9,18 +9,15 @@ using api.Application.Modules.TodoListModule.Domain.Models;
 namespace api.Application.Modules.TodoListModule.Features
 {
 
-    public record GetAllTodoListsQuery () : IRequest<List<TodoList>>;
+    public record GetAllTodoListsQuery() : IRequest<List<TodoList>>;
     public class GetAllTodoListsHandler : IRequestHandler<GetAllTodoListsQuery, List<TodoList>>
     {
         private readonly TodoListContext _db;
-
         public GetAllTodoListsHandler(TodoListContext db) => _db = db;
 
         public Task<List<TodoList>> Handle(GetAllTodoListsQuery cmd, CancellationToken cancellationToken)
         {
-           return Task.FromResult(_db.TodoLists.ToList());
+            return Task.FromResult(_db.TodoLists.ToList());
         }
-
-      
     }
 }
