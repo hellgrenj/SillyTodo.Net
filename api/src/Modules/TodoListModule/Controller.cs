59f2b8f4b435c2f3f630b0397bd672cc086cbf4d
hellgrenj <hellgrenj@gmail.com>
2021-05-31 13:26:02 +0200

    removed unused usings

--- api/src/Modules/TodoListModule/Controller.cs
+++ api/src/Modules/TodoListModule/Controller.cs
@@ -1,13 +1,9 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-
 using api.Modules.TodoListModule.Features;
 using api.Modules.TodoListModule.Domain.Models;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace api.Modules.TodoListModule
 {
