3ae8087e655bde0ab4c7e66f2bf1b02d75c5160c
hellgrenj <hellgrenj@gmail.com>
2021-11-10 08:25:42 +0100

    cleaned up test example a bit

---
+++ api/tests/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using System.Linq;
+global using System.Threading;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Logging;
+global using Moq;
+global using Xunit;
--- api/tests/Modules/TodoListModule/Features/CreateTodoListTests.cs
+++ api/tests/Modules/TodoListModule/Features/CreateTodoListTests.cs
@@ -1,41 +1,28 @@
 
-
-
-
-using System.Linq;
-using System.Threading;
-using api.Modules.TodoListModule;
-using api.Modules.TodoListModule.Features;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using Moq;
-using Xunit;
-
-namespace api.Modules.TodoListModule.Features.Tests
+namespace api.Modules.TodoListModule.Features.Tests;
+public class TodoListTests
 {
-    public class TodoListTests
+    DbContextOptions<TodoListContext> InMemoryOptions()
     {
-        DbContextOptions<TodoListContext> InMemoryOptions()
+        return new DbContextOptionsBuilder<TodoListContext>().UseInMemoryDatabase(databaseName: "Test_Database").Options;
+    }
+    ILogger<CreateTodoListHandler> GetMockedLogger()
+    {
+        return Mock.Of<ILogger<CreateTodoListHandler>>();
+    }
+    [Fact]
+    public async System.Threading.Tasks.Task CreateTodoList_creates_a_valid_todolistAsync()
+    {
+        using (var context = new TodoListContext(InMemoryOptions()))
         {
-            return new DbContextOptionsBuilder<TodoListContext>().UseInMemoryDatabase(databaseName: "Test_Database").Options;
-        }
-        ILogger<CreateTodoListHandler> GetMockedLogger()
-        {
-            return Mock.Of<ILogger<CreateTodoListHandler>>();
-        }
-        [Fact]
-        public async System.Threading.Tasks.Task CreateTodoList_creates_a_valid_todolistAsync()
-        {
-            using (var context = new TodoListContext(InMemoryOptions()))
-            {
-                var logger = GetMockedLogger();
-                var handler = new CreateTodoListHandler(context, logger);
-                var id = await handler.Handle(new CreateNewTodoListCommand("A new todo list"), default(CancellationToken));
-                
+            var logger = GetMockedLogger();
+            var handler = new CreateTodoListHandler(context, logger);
+            var id = await handler.Handle(new CreateNewTodoListCommand("A new todo list"), default(CancellationToken));
 
-                Assert.Equal(1, id);
-                Assert.Equal("A new todo list", context.TodoLists.Where(t => t.Id == 1).SingleOrDefault().Name);
-            }
+
+            Assert.Equal(1, id);
+            Assert.Equal("A new todo list", context.TodoLists.Where(t => t.Id == 1).SingleOrDefault().Name);
         }
     }
 }
+
